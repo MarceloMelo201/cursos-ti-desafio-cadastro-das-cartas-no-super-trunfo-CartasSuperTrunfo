615e5798e033b530c94afaa407d949e5a69192b4
Marcelo Melo <160616710+MarceloMelo201@users.noreply.github.com>
2025-09-03 21:27:12 +0000

    Commit desafio nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 
 int main() {
-    int populacao1, populacao2, numeroDePontos1, numeroDePontos2; 
+     int populacao1, populacao2, numeroDePontos1, numeroDePontos2; 
     float area1, area2, pib1, pib2; 
 
     printf("\n -- Super trunfo --"); 
@@ -34,6 +34,11 @@ int main() {
     printf("Numero de pontos turisticos: ");
     scanf("%d", &numeroDePontos2);
 
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
+    float pibPcap1 = pib1 / populacao1;
+    float pibPcap2 = pib2 / populacao2;
+    
     printf("\n -- Exibindo cartas -- \n");
 
     printf("\n -- Carta 1 -- \n");
@@ -42,6 +47,8 @@ int main() {
     printf("\nArea: %.2f", area1);
     printf("\nPIB: %.2f", pib1);
     printf("\nNumero de pontos turisticos: %d\n", numeroDePontos1);
+    printf("\nDensidade Populacional: %.2f\n", densidade1);
+    printf("\nPIB per Capita: %.2f\n", pibPcap1);
 
     printf("\n -- Carta 2 --\n");
 
@@ -49,10 +56,8 @@ int main() {
     printf("\nArea: %.2f", area2);
     printf("\nPIB: %.2f", pib2);
     printf("\nNumero de pontos turisticos: %d\n", numeroDePontos2);
-
-    return 0;
-
-
+    printf("\nDensidade Populacional: %.2f\n", densidade2);
+    printf("\nPIB per Capita: %.2f\n", pibPcap2);
 
     return 0;
 }
